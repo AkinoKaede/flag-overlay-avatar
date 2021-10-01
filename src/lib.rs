2fde57c4db0a03d647c4069cc62431415da10d9e
秋のかえで <autmaple@protonmail.com>
2021-10-01 14:01:16 +0800

    feat: flag-overlay-avatar

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+use image::{imageops, ImageBuffer, RgbaImage};
+
+fn flag_transparent(image: RgbaImage) -> RgbaImage {
+    let mut image = image;
+    let mut length = image.height();
+    if image.height() > image.width() {
+        length = image.width();
+    }
+
+    let mut sub_image = imageops::crop(&mut image, 0, 0, length, length).to_image();
+    let mut transparent_image = ImageBuffer::new(length, length);
+    for (x, y, pixel) in sub_image.enumerate_pixels_mut() {
+        let transparency =
+            255 - (255 as f64 * ((x * x + y * y) as f64).sqrt() / (length as f64)) as u8;
+        transparent_image.put_pixel(
+            x,
+            y,
+            image::Rgba([pixel[0], pixel[1], pixel[2], transparency]),
+        );
+    }
+
+    transparent_image
+}
+
+pub fn overlay(avatar: RgbaImage, flag: RgbaImage) -> RgbaImage {
+    let transparent_flag = flag_transparent(flag);
+    let mut length = avatar.height();
+    if avatar.height() > avatar.width() {
+        length = avatar.width();
+    }
+
+    let transparent_flag_resized = imageops::resize(
+        &transparent_flag,
+        length,
+        length,
+        imageops::FilterType::Nearest,
+    );
+
+    let mut avatar = avatar;
+
+    imageops::overlay(&mut avatar, &transparent_flag_resized, 0, 0);
+
+    avatar.clone()
+}
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use clap::{load_yaml, App};
+use flag_overlay_avatar::overlay;
+
+fn main() {
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from(yaml).get_matches();
+
+    let avatar = matches.value_of("avatar").unwrap();
+    let flag = matches.value_of("flag").unwrap();
+    let output = matches.value_of("output").unwrap();
+
+    let avatar_image = image::open(avatar).unwrap().to_rgba8();
+    let flag_image = image::open(flag).unwrap().to_rgba8();
+    let output_image = overlay(avatar_image, flag_image);
+
+    output_image.save(output).unwrap();
+}
